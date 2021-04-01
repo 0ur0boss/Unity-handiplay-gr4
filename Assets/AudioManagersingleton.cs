a04bdb159f2bdc1155a9b3493e4965bad4d51946
0ur0boss <enzo.duret@yahoo.fr>
2021-04-01 09:54:13 +0200

    Tilesheet+bug

--- Assets/AudioManagersingleton.cs
+++ Assets/AudioManagersingleton.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
 public class AudioManagersingleton : MonoBehaviour
 {
 
@@ -34,3 +35,4 @@ public class AudioManagersingleton : MonoBehaviour
     }
 
 }
+*/
