75b77c2d5a3a8d86d5ca0be14bec21a5d7cf5015
MisterM4 <mateo.simonnet@mmibordeaux.com>
2021-04-05 01:08:10 +0200

    le truc de enzo

--- Assets/Script/StopStick.cs
+++ Assets/Script/StopStick.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class StopStick : MonoBehaviour
 {
@@ -62,6 +63,8 @@ public class StopStick : MonoBehaviour
         }
             
         }*/
+
+        SceneManager.LoadScene("GameOver", LoadSceneMode.Additive);
     }
 
     }
